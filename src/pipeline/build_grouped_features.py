fb2b6810fdff950532eb1073ff346ea284bf3bdd
Aryton Tediarjo <atediarjo@sageitmbp1.local>
2020-02-13 12:28:26 -0800

    finished formatting all files

--- src/pipeline/build_grouped_features.py
+++ src/pipeline/build_grouped_features.py
@@ -2,72 +2,89 @@
 
 Author: Sage Bionetworks
 
-Script to group features by healthcodes, with several different aggregation types
+Script to group features by healthcodes,
+with several different aggregation
 
 """
 
-## import future libraries ## 
+# import future libraries
 from __future__ import print_function
 from __future__ import unicode_literals
 
-## import standard libraries ## 
+# import standard libraries
 import time
 import pandas as pd
 import numpy as np
 
-## import external libraries ##
+
+# import external libraries
 import synapseclient as sc
 
-## import project modules
+# import project modules
 from utils import query_utils as query
 
 
-## global variables ## 
-data_dict = {"GAIT_DATA": {"synId": "syn21575055"},
-            "GAIT_METADATA": {"synId": "syn21590710"},
-            "OUTPUT_INFO"      : {"parent_folder_synId"    : "syn21592268",
-                                "proj_repo_name"       : "mpower-gait-analysis",
-                                "path_to_github_token" : "~/git_token.txt"}
-            }
+# global variables
+data_dict = {
+    "GAIT_DATA": {"synId": "syn21575055"},
+    "GAIT_METADATA": {"synId": "syn21590710"},
+    "OUTPUT_INFO": {
+        "parent_folder_synId": "syn21592268",
+        "proj_repo_name": "mpower-gait-analysis",
+        "path_to_github_token": "~/git_token.txt"}
+}
 
 syn = sc.login()
+a = np.arange(start=7, stop=10)
+
 
 def iqr(x):
     """
     Function for getting IQR value
     """
     return q75(x) - q25(x)
+
+
 def q25(x):
     """
     Function for getting first quantile
     """
     return x.quantile(0.25)
+
+
 def q75(x):
     """
     Function for getting third quantile
     """
     return x.quantile(0.75)
+
+
 def valrange(x):
     """
     Function for getting the value range
     """
     return x.max() - x.min()
+
+
 def kurtosis(x):
     """
-    Function to retrieve kurtosis 
+    Function to retrieve kurtosis
     """
     return x.kurt()
+
+
 def skew(x):
     """
-    Function to retrieve skewness 
+    Function to retrieve skewness
     """
     return x.skew()
 
-def groupby_wrapper(data, group, exclude_columns = []):
+
+def groupby_wrapper(data, group, exclude_columns=[]):
     """
     Wrapper function to wrap feature data
-    into several aggregation function 
-    
+    into several aggregation function
+
     Args:
         data (dtype: pd.Dataframe)   : feature datasets
         group (dtype: string)        : which group to aggregate
@@ -77,12 +94,12 @@ def groupby_wrapper(data, group, exclude_columns = []):
         Returns grouped healthcodes features
     """
 
-    features = [feat for feat in data.columns if feat not in exclude_columns]    
-    data =  data[features].groupby(group).agg([np.max, 
-                                    np.median, 
-                                    np.mean,
-                                    q25, q75, 
-                                    valrange, iqr])
+    features = [feat for feat in data.columns if feat not in exclude_columns]
+    data = data[features].groupby(group).agg([np.max,
+                                              np.median,
+                                              np.mean,
+                                              q25, q75,
+                                              valrange, iqr])
     feature_cols = []
     for feat, agg in data.columns:
         feature_cols_name = "{}.{}".format(agg, feat)
@@ -90,43 +107,51 @@ def groupby_wrapper(data, group, exclude_columns = []):
     data.columns = feature_cols
     return data
 
+
 def main():
-    gait_data = query.get_file_entity(syn = syn, 
-                                      synid = data_dict["GAIT_DATA"]["synId"])
-    gait_data    = gait_data[gait_data["error_type"].isnull()]
-
-    gait_metadata = query.get_file_entity(syn = syn, 
-                                          synid = data_dict["GAIT_METADATA"]["synId"])
-
-    
+    gait_data = query.get_file_entity(
+        syn=syn,
+        synid=data_dict["GAIT_DATA"]["synId"])
+    gait_data = gait_data[gait_data["error_type"].isnull()]
+    gait_metadata = query.get_file_entity(
+        syn=syn,
+        synid=data_dict["GAIT_METADATA"]["synId"])
 
     metadata = ['appVersion', 'createdOn',
-                'phoneInfo', 'recordId', 
-                'table_version', 'test_type'] 
+                'phoneInfo', 'recordId',
+                'table_version', 'test_type']
 
-    output_mapping = {"walking_active_data" : gait_data[(gait_data["table_version"] != "MPOWER_PASSIVE") \
-                                                            & (gait_data["test_type"] == "walking")],
-                      "balance_active_data" : gait_data[(gait_data["table_version"] != "MPOWER_PASSIVE") \
-                                                            & (gait_data["test_type"] != "walking")],
-                      "passive_data": gait_data[(gait_data["table_version"] == "MPOWER_PASSIVE") \
-                                                            & (gait_data["test_type"] == "walking")]}
+    output_mapping = {"walking_active_data":
+                      gait_data[
+                          (gait_data["table_version"] != "MPOWER_PASSIVE")
+                          & (gait_data["test_type"] == "walking")],
+                      "balance_active_data":
+                      gait_data[
+                          (gait_data["table_version"] != "MPOWER_PASSIVE")
+                          & (gait_data["test_type"] != "walking")],
+                      "passive_data":
+                      gait_data[
+                          (gait_data["table_version"] == "MPOWER_PASSIVE")
+                          & (gait_data["test_type"] == "walking")]}
 
-    used_script_url = query.get_git_used_script_url(path_to_github_token = data_dict["OUTPUT_INFO"]["path_to_github_token"],
-                                                    proj_repo_name       = data_dict["OUTPUT_INFO"]["proj_repo_name"],
-                                                    script_name          = __file__)
+    used_script_url = query.get_git_used_script_url(
+        path_to_github_token=data_dict["OUTPUT_INFO"]["path_to_github_token"],
+        proj_repo_name=data_dict["OUTPUT_INFO"]["proj_repo_name"],
+        script_name=__file__)
 
     for data_name, dataframe in output_mapping.items():
         grouped_data = groupby_wrapper(dataframe, "healthCode", metadata)
-        grouped_data = pd.merge(grouped_data, gait_metadata, on = "healthCode", how = "inner")
-        query.save_data_to_synapse(syn = syn,
-                                    data = grouped_data,
-                                    used_script = used_script_url,
-                                    output_filename = "grouped_%s.csv"%data_name,
-                                    data_parent_id = "syn21537421")
+        grouped_data = pd.merge(
+            grouped_data, gait_metadata, on="healthCode", how="inner")
+        query.save_data_to_synapse(
+            syn=syn,
+            data=grouped_data,
+            used_script=used_script_url,
+            output_filename="grouped_%s.csv" % data_name,
+            data_parent_id="syn21537421")
 
-if __name__ ==  '__main__': 
+
+if __name__ == '__main__':
     start_time = time.time()
     main()
     print("--- %s seconds ---" % (time.time() - start_time))
-
-
--- src/pipeline/build_metadata.py
+++ src/pipeline/build_metadata.py
@@ -4,44 +4,45 @@ Author: Sage Bionetworks
 Script to gather all Demographics Data from all gait data from Synapse Table
 """
 
-## import future libraries ## 
+# import future libraries
 from __future__ import print_function
 from __future__ import unicode_literals
 
-## import standard libraries ## 
+# import standard libraries
 import time
 import pandas as pd
 import numpy as np
 
-## import external libraries ##
+# import external libraries
 import synapseclient as sc
 
-## import project modules
+# import project modules
 from utils import query_utils as query
 
-## global variables ## 
-data_dict = {"GAIT_FEATURE_DATA": {"synId": "syn21575055"},
-            "DEMO_DATA_V1"      : {"synId": "syn10371840"},
-             "DEMO_DATA_V2"     : {"synId": "syn15673379"},
-             "DEMO_DATA_EMS"    : {"synId": "syn10295288"},
-             "PROFILE_DATA_EMS" : {"synId": "syn10235463"},
-             "OUTPUT_INFO"      : {"active_metadata_filename"  : "active_gait_metadata.csv",
-                                    "passive_metadata_filename" : "passive_gait_metadata.csv",
-                                    "parent_folder_synId"       : "syn21537423",
-                                    "proj_repo_name"            : "mpower-gait-analysis",
-                                    "path_to_github_token"      : "~/git_token.txt"}
-    }
-
+# global variables
+data_dict = {
+    "GAIT_FEATURE_DATA": {"synId": "syn21575055"},
+    "DEMO_DATA_V1": {"synId": "syn10371840"},
+    "DEMO_DATA_V2": {"synId": "syn15673379"},
+    "DEMO_DATA_EMS": {"synId": "syn10295288"},
+    "PROFILE_DATA_EMS": {"synId": "syn10235463"},
+    "OUTPUT_INFO": {
+        "active_metadata_filename": "active_gait_metadata.csv",
+        "passive_metadata_filename": "passive_gait_metadata.csv",
+        "parent_folder_synId": "syn21537423",
+        "proj_repo_name": "mpower-gait-analysis",
+        "path_to_github_token": "~/git_token.txt"}
+}
 syn = sc.login()
 
-## helper functions ## 
+
 def annot_phone(params):
     """
-    Function to have lesser phone information choices
+    Function to have more concrete phone types
 
     Args:
         params (type: string): raw phone information
-    
+
     Returns:
         Rtype: String
         Returns an annotated dataset with lesser choice of phone information
@@ -62,105 +63,129 @@ def annot_phone(params):
         return "iPhone X"
     return params
 
+
 def generate_demographic_info(syn, feature_data):
     """
-    Function to generate unique healthcode demographic informations from Demographic and Profiles synapse table.
-    Takes in a dataframe containing healthcode, and join the table and compiled demographic 
+    Function to generate healthcode demographic informations
+    from Demographic and Profiles synapse table.
+    Takes in a dataframe containing healthcode,
+    and join the table and compiled demographic
     data by their healthcodes.
 
     Cleaning process:
-        >> Annotate controls, PD, MS in a column called class
-        >> Filter healthCode if gender, inferred diagnosis is NULL
-        >> Age, if recorded as birthYear will be based on current year - birthYear
-        >> healthCodes that has double PD status entry will be dropped from the dataframe
-        >> Age is subsetted between 18-120 years old
-        >> Aggregation of records for each healthcodes will be based on number of unique record entries,
-            other metadata features will be aggregated based on most frequent occurences
+        1. Annotate controls, PD, MS in a column called class
+        2. Filter healthCode if gender, inferred diagnosis is NULL
+        3. If age is recorded as birthYear, age
+            will be based on current year - birthYear
+        4. healthCodes that has double PD status entry
+            will be dropped from the dataframe
+        5. Age is subsetted between 18-120 years old
+        6. Aggregation of records for each healthcodes will be based on
+        number of unique record entries, other metadata features will be
+        aggregated based on most frequent occurences
 
     Args:
-        syn                : synapse object
-        data (pd.DataFrame): a pandas dataframe object 
-    
+        syn                : synapseclient object
+        data (pd.DataFrame): pandas dataframe
+
     Returns:
         RType: pd.DataFrame
-        Returns a dataframe of unique healthcode and its corresponding metadata features
+        Returns a dataframe of unique healthcode and
+        its corresponding metadata features
     """
 
-    ## demographics on mpower V1 ##
-    demo_data_v1 = syn.tableQuery("SELECT age, healthCode, inferred_diagnosis as PD, gender \
-                                 FROM {} where dataGroups NOT LIKE '%test_user%'"\
-                                .format(data_dict["DEMO_DATA_V1"]["synId"])).asDataFrame()
-    demo_data_v1 = demo_data_v1.dropna(subset = ["PD"], thresh = 1)                               
-    demo_data_v1["class"] = demo_data_v1["PD"].map({True :"PD", False:"control"})
+    # demographics on mpower V1
+    demo_data_v1 = syn.tableQuery(
+        "SELECT age, healthCode, \
+        inferred_diagnosis as PD, gender \
+        FROM {} where dataGroups \
+        NOT LIKE '%test_user%'"
+        .format(data_dict["DEMO_DATA_V1"]["synId"])).asDataFrame()
+    demo_data_v1 = demo_data_v1.dropna(subset=["PD"], thresh=1)
+    demo_data_v1["class"] = demo_data_v1["PD"].map(
+        {True: "PD", False: "control"})
 
+    # demographics on ElevateMS
+    demo_data_ems = syn.tableQuery(
+        "SELECT healthCode, dataGroups as MS,\
+        'gender.json.answer' as gender from {}\
+        where dataGroups NOT LIKE '%test_user%'"
+        .format(data_dict["DEMO_DATA_EMS"]["synId"])).asDataFrame()
+    profile_data_ems = syn.tableQuery(
+        "SELECT healthCode as healthCode, \
+        'demographics.age' as age from {}"
+        .format(data_dict["PROFILE_DATA_EMS"]["synId"])).asDataFrame()
+    demo_data_ems = pd.merge(
+        demo_data_ems, profile_data_ems, how="inner", on="healthCode")
+    demo_data_ems["class"] = demo_data_ems["MS"].map(
+        {"ms_patient": "MS", "control": "control"})
 
-    ## demographics on ElevateMS ##
-    demo_data_ems = syn.tableQuery("SELECT healthCode, dataGroups as MS,\
-                                'gender.json.answer' as gender from {}\
-                                where dataGroups NOT LIKE '%test_user%'"\
-                                .format(data_dict["DEMO_DATA_EMS"]["synId"])).asDataFrame()
-    profile_data_ems = syn.tableQuery("SELECT healthCode as healthCode, \
-                                    'demographics.age' as age from {}"\
-                                    .format(data_dict["PROFILE_DATA_EMS"]["synId"])).asDataFrame()
-    demo_data_ems = pd.merge(demo_data_ems, profile_data_ems, how = "inner", on = "healthCode")
-    demo_data_ems["class"] = demo_data_ems["MS"].map({"ms_patient":"MS", "control":"control"})
+    # demographics on mpower V2
+    demo_data_v2 = syn.tableQuery(
+        "SELECT birthYear, createdOn, healthCode, \
+        diagnosis as PD, sex as gender FROM {} \
+        where dataGroups NOT LIKE '%test_user%'"
+        .format(data_dict["DEMO_DATA_V2"]["synId"])).asDataFrame()
+    demo_data_v2 = demo_data_v2[demo_data_v2["PD"] != "no_answer"]
+    demo_data_v2["class"] = demo_data_v2["PD"].map(
+        {"parkinsons": "PD", "control": "control"})
+    demo_data_v2["birthYear"] = demo_data_v2[demo_data_v2["birthYear"].apply(
+        lambda x: True if x >= 0 else False)]
+    demo_data_v2["age"] = pd.to_datetime(
+        demo_data_v2["createdOn"], unit="ms").dt.year \
+        - demo_data_v2["birthYear"]
 
+    # concatenate all demographic data
+    demo_data = pd.concat(
+        [demo_data_v1, demo_data_v2, demo_data_ems], sort=False)\
+        .reset_index(drop=True)
 
-    ## demographics on mpower V2 ##
-    demo_data_v2 = syn.tableQuery("SELECT birthYear, createdOn, healthCode, \
-                                    diagnosis as PD, sex as gender FROM {} \
-                                    where dataGroups NOT LIKE '%test_user%'"\
-                                    .format(data_dict["DEMO_DATA_V2"]["synId"])).asDataFrame()
-    demo_data_v2              = demo_data_v2[demo_data_v2["PD"] != "no_answer"]               
-    demo_data_v2["class"]     = demo_data_v2["PD"].map({"parkinsons":"PD", "control":"control"})
-    demo_data_v2["birthYear"] = demo_data_v2[demo_data_v2["birthYear"].apply(lambda x: True if x>=0 else False)]
-    demo_data_v2["age"]       = pd.to_datetime(demo_data_v2["createdOn"], unit = "ms").dt.year - demo_data_v2["birthYear"]
-
-    ## concatenate all demographic data
-    demo_data = pd.concat([demo_data_v1, demo_data_v2, demo_data_ems], sort = False)\
-                            .reset_index(drop = True)
-
-    ## filter gender ## 
+    # filter gender
     demo_data["gender"] = demo_data["gender"].str.lower()
-    demo_data = demo_data[(demo_data["gender"] == "female") | (demo_data["gender"] == "male")]
+    demo_data = demo_data[(demo_data["gender"] == "female")
+                          | (demo_data["gender"] == "male")]
 
-    ## filter age ##
+    # filter age
     demo_data["age"] = demo_data["age"].apply(lambda x: float(x))
-    demo_data        = demo_data[(demo_data["age"] <= 120) & (demo_data["age"] >= 18)]
-    demo_data        = demo_data[~demo_data["age"].isin([np.inf, -np.inf])]
-    demo_data        = demo_data.sort_values(by = "age", ascending = False)
+    demo_data = demo_data[(demo_data["age"] <= 120) & (demo_data["age"] >= 18)]
+    demo_data = demo_data[~demo_data["age"].isin([np.inf, -np.inf])]
+    demo_data = demo_data.sort_values(by="age", ascending=False)
 
+    # check if multiple input of any class
+    demo_data = pd.merge(demo_data,
+                         (demo_data.groupby("healthCode")
+                          .nunique()["class"] >= 2)
+                         .reset_index()
+                         .rename({"class": "has_double_class_entry"}, axis=1),
+                         on="healthCode",
+                         how="left")
+    demo_data = demo_data.drop(
+        ["PD", "MS", "birthYear",
+         "createdOn", "has_double_class_entry"], axis=1)
+    demo_data = demo_data.drop_duplicates(
+        'healthCode', keep="first").reset_index(drop=True)
 
-    ## filter double entry ##
-    ## check if multiple input of any class ##
-    demo_data = pd.merge(demo_data, 
-        (demo_data.groupby("healthCode")\
-        .nunique()["class"] >= 2)\
-            .reset_index()\
-                .rename({"class":"has_double_class_entry"}, axis = 1),
-        on = "healthCode", 
-        how = "left")
-    demo_data = demo_data.drop(["PD", "MS", "birthYear", "createdOn", "has_double_class_entry"], axis = 1)
-    demo_data = demo_data.drop_duplicates('healthCode', keep = "first").reset_index(drop = True)
+    # merge dataframe with demographic data
 
-    ## merge dataframe with demographic data ##
-    ## synId
-    data         = pd.merge(feature_data, demo_data, how = "inner", on = "healthCode")
-    
-    ## aggregation of metadata features ##
-    data= data[["recordId", "healthCode", "phoneInfo", "age", "gender", "class", "table_version"]].groupby(["healthCode"])\
-                .agg({"recordId": pd.Series.nunique,
-                     "phoneInfo": pd.Series.mode,
-                     "class": pd.Series.mode,
-                     "gender": pd.Series.mode,
-                     "age": pd.Series.mode,
-                     "table_version": pd.Series.mode})
-    data = data.rename({"recordId":"nrecords"}, axis = 1)
-    
-    ## annotate phone into simpler categories ##
-    data["phoneInfo"] = data["phoneInfo"].apply(lambda x: x[0] if not isinstance(x,str) else x)
+    data = pd.merge(feature_data, demo_data, how="inner", on="healthCode")
+
+    # aggregation of metadata features
+    data = data[["recordId", "healthCode", "phoneInfo", "age",
+                 "gender", "class", "table_version"]].groupby(["healthCode"])\
+        .agg({"recordId": pd.Series.nunique,
+              "phoneInfo": pd.Series.mode,
+              "class": pd.Series.mode,
+              "gender": pd.Series.mode,
+              "age": pd.Series.mode,
+              "table_version": pd.Series.mode})
+    data = data.rename({"recordId": "nrecords"}, axis=1)
+
+    # annotate phone into simpler categories
+    data["phoneInfo"] = data["phoneInfo"].apply(
+        lambda x: x[0] if not isinstance(x, str) else x)
     data["phoneInfo"] = data["phoneInfo"].apply(annot_phone)
     return data
+
 
 def main():
     """
@@ -169,32 +194,40 @@ def main():
     Note: Passive gait data will be separated from active gait data
           as we dont want to combine both in analysis
     """
-    ## get featurized gait data ##
-    gait_data    = query.get_file_entity(syn = syn, synid = data_dict["GAIT_FEATURE_DATA"]["synId"])
-    
-    ## filter error data, and separate active and passive data ##
-    gait_data    = gait_data[gait_data["error_type"].isnull()]
-    active_data  = gait_data[gait_data["table_version"] != "MPOWER_PASSIVE"]
-    passive_data = gait_data[gait_data["table_version"] == "MPOWER_PASSIVE"]
-    
-    output_mapping = {"active_metadata" : generate_demographic_info(syn, active_data), 
-                      "passive_metadata" : generate_demographic_info(syn, passive_data)}
-    
-    ## get this script URL ## 
-    used_script_url = query.get_git_used_script_url(path_to_github_token = data_dict["OUTPUT_INFO"]["path_to_github_token"],
-                                                    proj_repo_name       = data_dict["OUTPUT_INFO"]["proj_repo_name"],
-                                                    script_name          = __file__)
+    # get featurized gait data
+    gait_data = query.get_file_entity(
+        syn=syn, synid=data_dict["GAIT_FEATURE_DATA"]["synId"])
 
-    ## save active data to synapse ##
+    # filter error data, and separate active and passive data
+    gait_data = gait_data[gait_data["error_type"].isnull()]
+    active_data = gait_data[gait_data["table_version"] != "MPOWER_PASSIVE"]
+    passive_data = gait_data[gait_data["table_version"] == "MPOWER_PASSIVE"]
+
+    output_mapping = {"active_metadata":
+                      generate_demographic_info(syn, active_data),
+                      "passive_metadata":
+                      generate_demographic_info(syn, passive_data)}
+
+    # get this script git blob URL
+    used_script_url = query.get_git_used_script_url(
+        path_to_github_token=data_dict["OUTPUT_INFO"]["path_to_github_token"],
+        proj_repo_name=data_dict["OUTPUT_INFO"]["proj_repo_name"],
+        script_name=__file__)
+
+    # save active data to synapse
     for data_name, dataframe in output_mapping.items():
-        query.save_data_to_synapse(syn = syn,
-                                    data = dataframe,
-                                    source_table_id = [values["synId"] for key, values in data_dict.items() if key != "OUTPUT_INFO"],
-                                    used_script = used_script_url,  
-                                    output_filename = "%s.csv"%data_name,
-                                    data_parent_id  = data_dict["OUTPUT_INFO"]["parent_folder_synId"])
-    
-if __name__ ==  '__main__': 
+        query.save_data_to_synapse(
+            syn=syn,
+            data=dataframe,
+            source_table_id=[values["synId"] for key,
+                             values in data_dict.items()
+                             if key != "OUTPUT_INFO"],
+            used_script=used_script_url,
+            output_filename="%s.csv" % data_name,
+            data_parent_id=data_dict["OUTPUT_INFO"]["parent_folder_synId"])
+
+
+if __name__ == '__main__':
     start_time = time.time()
     main()
     print("--- %s seconds ---" % (time.time() - start_time))
