7dd7395a21c4a0a8050b5de36464214b6a87121c
Aryton Tediarjo <atediarjo@sageitmbp1.local>
2020-02-13 17:09:43 -0800

    remove private methods

--- src/pipeline/utils/gait_features_utils.py
+++ src/pipeline/utils/gait_features_utils.py
@@ -152,7 +152,8 @@ class GaitFeaturize:
                     data["z"] = data[sensor].apply(lambda x: x["z"])
                     data = data.drop([sensor], axis=1)
         # exceptions during data reading
-        except (AttributeError, TypeError, FileNotFoundError, MemoryError, KeyError) as err:
+        except (AttributeError, TypeError, FileNotFoundError,
+                MemoryError, KeyError) as err:
             data = "ERROR: %s" % type(err).__name__
         else:
             # check if empty
@@ -165,7 +166,7 @@ class GaitFeaturize:
         finally:
             return data
 
-    def _format_time_series_data(self, data):
+    def format_time_series_data(self, data):
         """
         Utility function to format time series
 
@@ -202,7 +203,7 @@ class GaitFeaturize:
         data = data.sort_index()
         return data[["td", "x", "y", "z", "AA"]]
 
-    def _calculate_freeze_index(self, accel_series, sample_rate):
+    def calculate_freeze_index(self, accel_series, sample_rate):
         """
         Modified pdkit FoG freeze index function to be compatible with
         current data gait pipeline
@@ -299,7 +300,7 @@ class GaitFeaturize:
                 start = crossing
         return rotation_dict
 
-    def _split_gait_dataframe_to_chunks(self, accel_dataframe, periods):
+    def split_gait_dataframe_to_chunks(self, accel_dataframe, periods):
         """
         Function to chunk dataframe into several periods of rotational
         and non-rotational sequences into List.
@@ -362,9 +363,9 @@ class GaitFeaturize:
                  "chunk": "walk_chunk_%s" % num_walk_window})
         return chunk_list
 
-    def _featurize_gait_dataframe_chunks_by_window(self,
-                                                   list_of_dataframe_dicts,
-                                                   rotation_dict):
+    def featurize_gait_dataframe_chunks_by_window(self,
+                                                  list_of_dataframe_dicts,
+                                                  rotation_dict):
         """
         Function to featurize list of rotation-segmented dataframe chunks with
         moving windows, and returns list of dictionary with all the pdkit
@@ -533,7 +534,7 @@ class GaitFeaturize:
 
             speed_of_gait = gp.speed_of_gait(series, wavelet_level=6)
             energy_freeze_index, \
-                locomotor_freeze_index = self._calculate_freeze_index(
+                locomotor_freeze_index = self.calculate_freeze_index(
                     series, self.sampling_frequency)
 
             feature_dict["%s_steps" % axis] = steps
@@ -597,9 +598,9 @@ class GaitFeaturize:
         resampled_accel = self.resample_signal(accel_data)
         rotation_dict = self.get_gait_rotation_info(resampled_rotation)
         periods = [v["period"] for k, v in rotation_dict.items()]
-        list_df_chunks = self._split_gait_dataframe_to_chunks(
+        list_df_chunks = self.split_gait_dataframe_to_chunks(
             resampled_accel, periods)
         list_of_feature_dictionary = \
-            self._featurize_gait_dataframe_chunks_by_window(
+            self.featurize_gait_dataframe_chunks_by_window(
                 list_df_chunks, rotation_dict)
         return list_of_feature_dictionary
