ac88fc36aac43f14701d5815e4dee3c679e1e74c
arytontediarjo <35942857+arytontediarjo@users.noreply.github.com>
2020-02-27 18:03:53 -0800

    fixed setup py typos

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='src',
     packages=find_packages(),
     version='0.1.0',
     description='A repository of gait data featurization, exploration and data modelling',
     author='Aryton-SageBionetworks',
     license='MIT',
-    install_requrires = required,
+    install_requires = required,
 )
